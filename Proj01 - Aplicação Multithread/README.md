64c2b87b7b60677679fae45c93c27f1423684576
Leonardo Rener <leonardo-rener@hotmail.com>
2022-04-09 17:13:33 -0300

    Projeto 01 - Finalizado

---
+++ Proj01 - Aplicação Multithread/README.md
@@ -0,0 +1,20 @@
+# Proj01 - Aplicação Multithread
+## Caça-Palavras
+
+Este programa utiliza threads durante a busca por palavras em um caça-palavras, para isso são necessarias 3 thread, suas funções são:
+- buscar palavras na horizontal
+- buscar palavras na vertical
+- buscar palavras na diagonal
+
+Para utilizar o programa devemos abrir o arquivo execultavel.
+
+Os parametros necessarios iniciais são: 
+- Tamanho da matriz do caça-palavras (n x n)
+- Modo de geração da matriz ("A" para automatico e "M" para manual)
+- Para o modo manual ("M") é necessario digitar a matriz de caracteres
+
+> ![Entrada](imagens/entrada-caca-palavras.png)
+
+Depois basta digitar as palavras que encontrar, para sair digite "0".
+
+> ![Saida](imagens/saida-caca-pavaras.png)
--- Proj01 - Aplicação Multithread/main.c
+++ Proj01 - Aplicação Multithread/main.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_PALAVRA 100
 #define MAX_THREADS 3
 
 #define ANSI_COLOR_RED   "\x1B[31m"
@@ -24,9 +23,10 @@ typedef struct buscaPalavra_args {
     char *palavra;
 } buscaPalavra_args;
 
-int obterTamanhoMatriz();
-void gerarMatriz(char **matriz, int n);
+void menuInicial(int *n, char *modo);
+char** gerarMatriz(int n, char modo);
 void printMatriz(char **matriz, int n);
+void recebePalavra(int n, char *palavra);
 void* buscaPalavraHorizontal(void *arguments);
 void* buscaPalavraVertical(void *arguments);
 void* buscaPalavraDiagonal(void *arguments);
@@ -34,42 +34,30 @@ void* buscaPalavraDiagonal(void *arguments);
 int main()
 {
     int n;
+    char modo;
     char **matriz;
-    char palavra[MAX_PALAVRA];
+    char *palavra;
     pthread_t thread[MAX_THREADS];
 
     printf(ANSI_COLOR_BLU "\nBEM VINDO! - ");
     printf(ANSI_COLOR_RED "CAÇA-PALAVRAS - ");
     printf(ANSI_COLOR_BLU "MC504\n\n" ANSI_COLOR_RESET);
     
-    // obtem o valor de n.
-    n = obterTamanhoMatriz();
-    
-    // aloca um vetor de n ponteiros para linhas.
-    matriz = malloc (n * sizeof (char*)) ;
-    // aloca cada uma das linhas vetores de n inteiros.
-    for(int i = 0; i < n; i++)
-        matriz[i] = malloc (n * sizeof (char));
-        
+    // Obtem o valor de n e o modo de geracao do caca-palavras.
+    menuInicial(&n, &modo);
+
     // Preenche a matriz com caracters aleatorios.
-    gerarMatriz(matriz, n);
+    matriz = gerarMatriz(n, modo);
     
     // Mostra a matriz atual.
     printMatriz(matriz, n);
 
-    while(1){
-        printf(ANSI_COLOR_GRN "Digite uma palavra (Ou '0' para encerrar): " ANSI_COLOR_WHT);
-        scanf("%s", palavra);
-        printf("\n");
-        if(strlen(palavra) > n){
-            printf(ANSI_COLOR_MAG "palavra invalida!");
-        }
+    // Recebe a palavra do usuario.
+    recebePalavra(n, palavra);
+    printf(ANSI_COLOR_MAG "%s\n", palavra);
 
-        if(strcmp(palavra, "0") == 0){
-            printf(ANSI_COLOR_MAG "Programa finalizado!\n\n");
-            break;
-        }
-
+    while(strcmp(palavra, "0") != 0){
+        // Cria os argumentos para as threads de busca de palavra.
         buscaPalavra_args *args = malloc(sizeof(buscaPalavra_args));
         args->matriz = matriz;
         args->n = n;
@@ -83,37 +71,72 @@ int main()
         pthread_join(thread[1], NULL); 
         pthread_join(thread[2], NULL); 
 
+        // Mostra a matriz atual.
         printMatriz(matriz, n);
+
+        // Recebe a palavra do usuario.
+        recebePalavra(n, palavra);
     }
+
+    printf(ANSI_COLOR_MAG "Programa finalizado!\n\n");
 
     free(matriz);
     
     return 0;
 }
 
-int obterTamanhoMatriz()
+void menuInicial(int *n, char *modo)
 {
-    int n;
+    int entrada_n;
+
     while(1){
         printf(ANSI_COLOR_YEL "Digite o tamanho do caça-palavras (n x n): " ANSI_COLOR_WHT);
-        scanf("%d", &n);
-        if(n < 10 || n > 100){
-            printf(ANSI_COLOR_MAG "Tamanho invalido! Digite um valor entre 10 e 100\n");
+        scanf("%d", &entrada_n);
+        if(entrada_n >= 10 && entrada_n <= 100){
+            while(1){
+                printf(ANSI_COLOR_GRN "Digite \"A\" para gerar um caca-palavras aleatorio, ou \"M\" para adicionar manualmente: " ANSI_COLOR_WHT);
+                scanf("%s", modo);
+                if(tolower(*modo) == 'a' || tolower(*modo) == 'm'){
+                    *n = entrada_n;
+                    return;
+                }
+                else
+                {
+                    printf(ANSI_COLOR_MAG "Modo invalido! Digite novamente...\n");
+                }
+            }
         }
-        else{
-            return n;
-        }
+        printf(ANSI_COLOR_MAG "Tamanho invalido! Digite um valor entre 10 e 100\n");
     }
+    
 }
 
-void gerarMatriz(char **matriz, int n)
+char** gerarMatriz(int n, char modo)
 {
     int i, j;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < n; j++){
-            matriz[i][j] = 'a' + (char)(rand()%26);
+    char **matriz;
+
+    // aloca um vetor de n ponteiros para linhas.
+    matriz = malloc (n * sizeof (char*));
+    // aloca cada uma das linhas vetores de n inteiros.
+    for(int i = 0; i < n; i++)
+        matriz[i] = malloc (n * sizeof (char));
+
+    if(tolower(modo) == 'a'){
+        for(i = 0; i < n; i++){
+            for(j = 0; j < n; j++){
+                matriz[i][j] = 'a' + (char)(rand()%26);
+            }
         }
     }
+    else{
+        printf(ANSI_COLOR_YEL "Digite as letras do caca-palavras (digite uma linha por vez):\n" ANSI_COLOR_WHT);
+        for (i = 0; i < n; i++){
+            scanf("%s", matriz[i]);
+        }
+    }
+
+    return matriz;
 }
 
 void printMatriz(char **matriz, int n)
@@ -133,6 +156,21 @@ void printMatriz(char **matriz, int n)
         printf("\n");
     }
     printf(ANSI_COLOR_RESET "\n");
+}
+
+void recebePalavra(int n, char *palavra)
+{
+    while(1){
+        printf(ANSI_COLOR_GRN "Digite uma palavra (Ou '0' para encerrar): " ANSI_COLOR_WHT);
+        scanf("%s", palavra);
+        printf("\n");
+        if(strlen(palavra) > n){
+            printf(ANSI_COLOR_MAG "palavra invalida!");
+        }
+        else{
+            return;
+        }
+    }
 }
 
 void* buscaPalavraHorizontal(void *arguments)
